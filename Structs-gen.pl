5cc3edfb58d23c5b8db16951c22e459cc21299e8
Pavel Plesov <pavel.plesov@gmail.com>
2014-05-19 23:24:54 +0400

    WIP: struct body, introduce vector

--- Structs-gen.pl
+++ Structs-gen.pl
@@ -3,6 +3,15 @@
 use strict;
 use Data::Dumper;
 
+my %options = (
+	namespace => "",
+	#namespace => "foospace",
+
+	# pragma|define
+	includeonce => "pragma",
+	#includeonce => "define",
+);
+
 usage($0) unless $#ARGV+1;
 
 my %structs;
@@ -11,14 +20,25 @@ foreach my $file (@ARGV) { parse_file($file) };
 
 print Dumper(\%structs);
 
-foreach my $index (keys %structs)
+foreach my $index (sort { $a cmp $b } keys %structs)
 {
-	my %ready = generate_struct($structs{$index});
-	foreach my $file (keys %ready)
+	my %ready = generate($structs{$index});
+	foreach my $file (sort { $a cmp $b } keys %ready)
 	{
 		write_file($file, $ready{$file});
 	}
 }
+
+sub generate($)
+{
+	my %struct = %{$_[0]};
+
+	return generate_struct(\%struct) if $struct{type} eq 'struct';
+	return generate_vector(\%struct) if $struct{type} eq 'vector';
+
+	()
+}
+
 
 sub parse_file()
 {
@@ -41,12 +61,28 @@ sub parse_file()
 			print "%struct($1) start\n";
 			%one = (
 				name => $1,
+				type => 'struct',
 				fields => [],
 				comments => [],
 				includes => [],
 				methods => [],
 				source => $SOURCE,
 			);
+			next;
+		}
+
+		if ($line =~ /^vector ([^ ]+) : ([^; ]+)/)
+		{
+			print "%vector($1)\n";
+			my %v = (
+				name => $1,
+				value => $2,
+				type => 'vector',
+				comments => [],
+				includes => [],
+				source => $SOURCE,
+			);
+			$structs{$v{name}} = { %v };
 			next;
 		}
 
@@ -84,23 +120,24 @@ sub generate_struct($)
 {
 	my %struct = %{$_[0]};
 	my %auto_inc;
-	my $auto =
-qq{//
-// This file is automaticaly generated from $struct{source}
-//
-};
 
 	my $fields_def = join("\n\t", map {
 		$auto_inc{$_->{type}} = 1 if defined($structs{$_->{type}});
 		"$_->{type} $_->{name};"
 	} @{$struct{fields}});
 
-	my $header = $auto
+	my $init_fields_default;
+
+	my $header = ""
+		.get_include_single_start(\%struct)
+		.get_auto_comment(\%struct)
 		.join("\n", @{$struct{includes}})
 		.join("\n", map { get_include_type($_) } keys %auto_inc)
+		.get_namespace_start(\%struct)
 		.qq{
 
-struct $struct{name} \{
+struct $struct{name}
+\{
 	$fields_def
 
 	$struct{name}();
@@ -115,15 +152,84 @@ struct $struct{name} \{
 	bool operator!=(const $struct{name}& rhs) const;
 	void swap($struct{name}& rhs);
 \};
-};
+}
+		.get_namespace_stop(\%struct)
+		.get_include_single_stop(\%struct)
+		;
 
-	my $body;
+	my $body = ""
+		.get_auto_comment(\%struct)
+		.get_namespace_start(\%struct)
+.qq{
+$struct{name}::$struct{name}()
+	$init_fields_default
+{
+}
+
+$struct{name}::$struct{name}(const $struct{name}& rhs)
+{
+}
+
+$struct{name}& $struct{name}::operator=(const $struct{name}& rhs)
+{
+}
+
+bool $struct{name}::operator<(const $struct{name}& rhs) const
+{
+}
+
+bool $struct{name}::operator>(const $struct{name}& rhs) const
+{
+}
+
+bool $struct{name}::operator==(const $struct{name}& rhs) const
+{
+}
+
+bool $struct{name}::operator!=(const $struct{name}& rhs) const
+{
+}
+
+void $struct{name}::swap($struct{name}& rhs)
+{
+}
+
+}
+		.get_namespace_stop(\%struct)
+		;
 
 	return (
-		$struct{name}.".h" => $header,
-		$struct{name}.".cpp" => $body,
+		get_file_name_header(\%struct) => $header,
+		get_file_name_source(\%struct) => $body,
 	)
 }
+
+sub generate_vector($)
+{
+	my %struct = %{$_[0]};
+	my %auto_inc = (
+		$struct{value} => 1,
+	);
+
+	my $header = ""
+		.get_include_single_start(\%struct)
+		.get_auto_comment(\%struct)
+		.join("\n", map { get_include_type($_) } keys %auto_inc)
+		.get_namespace_start(\%struct)
+		.qq{
+#include <vector>
+
+typedef std::vector<$struct{value}> $struct{name};
+}
+		.get_namespace_stop(\%struct)
+		.get_include_single_stop(\%struct)
+		;
+
+	return (
+		get_file_name_header(\%struct) => $header,
+	)
+}
+
 
 sub usage($)
 {
@@ -147,7 +253,97 @@ sub write_file($:$)
 	print "ok\n";
 }
 
+sub get_include_type_path($)
+{
+	$_[0].".h"
+}
+
 sub get_include_type($)
 {
-	"#include \"$_[0].h\""
+	"#include \"".get_include_type_path($_[0])."\""
+}
+
+sub get_auto_comment($)
+{
+	my %struct = %{$_[0]};
+
+qq{//
+// This file is automaticaly generated from $struct{source}
+//
+};
+}
+
+sub get_file_name_header($)
+{
+	my %struct = %{$_[0]};
+
+	$struct{name}.".h"
+}
+
+sub get_file_name_source($)
+{
+	my %struct = %{$_[0]};
+
+	$struct{name}.".cpp"
+}
+
+sub get_include_single_mark($)
+{
+	my %struct = %{$_[0]};
+	my $mark = "STRUCTGEN_"
+		.$options{namespace}
+		."_"
+		.get_include_type_path($struct{name})
+		."_INDLUDED";
+	$mark =~ s/[^a-zA-Z0-0_]/_/g;
+
+	$mark
+}
+
+
+sub get_include_single_start($)
+{
+	my %struct = %{$_[0]};
+
+	return "#pragma once\n\n" if $options{includeonce} eq "pragma";
+
+	my $mark = get_include_single_mark(\%struct);
+
+qq{#ifndef $mark
+#define $mark
+
+}
+
+}
+
+sub get_include_single_stop($)
+{
+	my %struct = %{$_[0]};
+
+	return "" if $options{includeonce} eq "pragma";
+
+	my $mark = get_include_single_mark(\%struct);
+
+qq{
+#endif // #ifndef $mark
+}
+
+}
+
+sub get_namespace_start($)
+{
+	my %struct = %{$_[0]};
+
+	return "" if length($options{namespace}) < 1;
+
+	"\n\nnamespace $options{namespace}\n{\n"
+}
+
+sub get_namespace_stop($)
+{
+	my %struct = %{$_[0]};
+
+	return "" if length($options{namespace}) < 1;
+
+	"\n\n} // namespace $options{namespace}\n\n"
 }
